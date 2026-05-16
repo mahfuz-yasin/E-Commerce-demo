00b37723db70f824162a83f73a97ed38725bea6c
mahfuz yasin <mahfuzyasin21@gmail.com>
2026-05-16 08:19:37 +0600

    Temporarily disable Redux store for debugging

--- components/Application/GlobalProvider.jsx
+++ components/Application/GlobalProvider.jsx
@@ -1,10 +1,9 @@
 'use client'
 import React, { Suspense } from 'react'
-import { Provider } from 'react-redux'
 import Loading from './Loading'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { store } from '@/store/client-store'
+// import { store } from '@/store/client-store'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -18,19 +17,21 @@ const queryClient = new QueryClient({
 })
 
 const GlobalProvider = ({ children }) => {
-    if (!store) {
-        console.error('Redux store is undefined')
-        return <div>Error: Store not initialized</div>
-    }
+    // Temporarily disabled Redux store for debugging
+    // if (!store) {
+    //     console.error('Redux store is undefined')
+    //     return <div>Error: Store not initialized</div>
+    // }
 
     return (
         <QueryClientProvider client={queryClient}>
-            <Provider store={store}>
+            {/* Temporarily disabled Redux Provider for debugging */}
+            {/* <Provider store={store}> */}
                 <Suspense fallback={<Loading />}>
                     {children}
                     <ToastContainer />
                 </Suspense>
-            </Provider>
+            {/* </Provider> */}
             <Suspense fallback={null}>
                 <ReactQueryDevtools initialIsOpen={false} />
             </Suspense>
